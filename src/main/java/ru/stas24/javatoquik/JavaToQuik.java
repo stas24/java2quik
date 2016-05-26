4448a09fed721df1f5f0ce998924cdbae32e73b2
Stanislav <Stanislav@stason-pc.mshome.net>
2016-05-26 03:06:07 +0300

    Отправка различных заявок. Обрабатываются только новые строки из входящих данных.

--- src/main/java/ru/stas24/javatoquik/JavaToQuik.java
+++ src/main/java/ru/stas24/javatoquik/JavaToQuik.java
@@ -14,6 +14,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,11 +23,9 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  *
@@ -37,8 +36,26 @@ public abstract class JavaToQuik {
     private final String quikPath;
     private final String troName = "output.tro";
     private final String triName = "input.tri";
+    private String account;
+    private String clientCode;
     Thread watcherThread;
     PrintWriter output;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getClientCode() {
+        return clientCode;
+    }
+
+    public void setClientCode(String clientCode) {
+        this.clientCode = clientCode;
+    }
     
     public JavaToQuik(String quikPath) throws IOException {
         this.quikPath = quikPath;
@@ -50,6 +67,7 @@ public abstract class JavaToQuik {
         try (FileWriter file = new FileWriter(quikPath + File.separator + triName)) {
             file.write("");
         }
+        readedLines = 0;
         
         // Input file (input for Quik)
         FileWriter fw = new FileWriter(quikPath + File.separator + triName, true);
@@ -63,12 +81,13 @@ public abstract class JavaToQuik {
     abstract protected void onTransactionResult(Integer transactionId, Map args);
     abstract protected void onStop(String reason);
     
+    // Количество прочитанных строк из файла troName, используется для чтения только новых строк
+    private int readedLines;
+    
     private void startWatcher() {
         // Запустить поток, который будет отслеживать изменения в файле troName
         
         watcherThread = new Thread(new Runnable() {
-            
-            private final Set<Integer> processedTransactions = new HashSet<>();
             
             @Override
             public void run() {
@@ -92,9 +111,14 @@ public abstract class JavaToQuik {
                                     LinkedList<String> lines = new LinkedList<>();
                                     try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(quikPath + File.separator + event.context().toString()), StandardCharsets.UTF_8))) {
                                         String line;
+                                        int lineIndex = 0;
                                         while ((line = bufferedReader.readLine()) != null) {
-                                            lines.add(line);
+                                            if (lineIndex >= readedLines - 1) {
+                                                lines.add(line);
+                                            }
+                                            lineIndex++;
                                         }
+                                        readedLines = lineIndex;
                                     } catch (FileNotFoundException ex) {
                                         System.out.println("Error: " + ex.toString());
                                     }
@@ -121,13 +145,8 @@ public abstract class JavaToQuik {
                                             transactionId = -1;
                                         }
                                         
-                                        // Вызвать общий callback только еслии такой заявки еще не было
-                                        if ( ! processedTransactions.contains(transactionId)) {
-                                            onTransactionResult(transactionId, args);
-                                            processedTransactions.add(transactionId);
-                                        } else {
-                                            System.out.println("Transaction already processed: " + transactionId + " " + args.get("TRANS_ID"));
-                                        }
+                                        // Вызвать callback
+                                        onTransactionResult(transactionId, args);
                                     }
                                 }
                             }
@@ -150,13 +169,64 @@ public abstract class JavaToQuik {
         watcherThread.start();
     }
     
-    public void sendTransaction(String transaction) {
-        output.println(transaction);
-        output.flush();
-    }
-    
     public void stop() {
         watcherThread.interrupt();
+    }
+    
+    // Уникальный номер заявки, фактически - это количество отправленных заявок
+    int transId = 0;
+    protected final int sendTransaction(String transaction) {
+        transId++;
+        output.println("TRANS_ID=" + transId + "; " + transaction);
+        output.flush();
+        return transId;
+    }
+    
+    public enum Operation {
+        BUY('B'), SELL('S');
+        
+        private final char operationCode;
+        Operation(char operationCode) {
+            this.operationCode = operationCode;
+        }
+        public char getValue() {
+            return operationCode;
+        }
+    }
+    
+    /*
+    TYPE - Тип заявки, необязательный параметр. Значения: «L» – лимитированная (по умолчанию), «M» – рыночная
+    OPERATION - Направление заявки, обязательный параметр. Значения: «S» – продать, «B» – купить
+    Пример:
+    ACCOUNT=4100WWQ; CLIENT_CODE=4100WWQ; TYPE=M; TRANS_ID=8; CLASSCODE=SPBFUT; SECCODE=SRM6; ACTION=NEW_ORDER; OPERATION=B; PRICE=16231; QUANTITY=15;
+    */
+    public int sendOrder(String classCode, String secCode, Operation operation, BigDecimal price, int quantity) {
+        return sendTransaction("ACCOUNT=" + getAccount() +
+                "; CLIENT_CODE=" + getClientCode() +
+                "; TYPE=M; CLASSCODE=" + classCode +
+                "; SECCODE=" + secCode +
+                "; ACTION=NEW_ORDER; OPERATION=" + operation +
+                "; PRICE=" + price.toPlainString() +
+                "; QUANTITY=" + quantity + ";");
+    }
+    
+    /*
+    Снятие всех заявок
+    Пример:
+    TRANS_ID=1; CLASSCODE=TQBR; ACTION=KILL_ALL_ORDERS; CLIENT_CODE=Q6;
+    */
+    public int sendKillAllOrders(String classCode) {
+        return sendTransaction("CLASSCODE=" + classCode + "; ACTION=KILL_ALL_ORDERS; CLIENT_CODE=" + getClientCode() + ";");
+    }
+
+    /*
+    Перестановка заявок на срочном рынке FORTS
+    Пример:
+    ACTION=MOVE_ORDERS; TRANS_ID=333; CLASSCODE=SPBFUT; SECCODE=EBM6; FIRM_ID=SPBFUT389; MODE=1; FIRST_ORDER_NUMBER=21445064; FIRST_ORDER_NEW_PRICE=10004; FIRST_ORDER_NEW_QUANTITY=4; SECOND_ORDER_NUMBER=21445065; SECOND_ORDER_NEW_PRICE=10004; SECOND_ORDER_NEW_QUANTITY=4;
+    */
+    public int sendMoveOrders() {
+        // TODO: Вставить параметры транзакции из аргументов метода
+        return sendTransaction("ACTION=MOVE_ORDERS; CLASSCODE=SPBFUT; SECCODE=EBM6; FIRM_ID=SPBFUT389; MODE=1; FIRST_ORDER_NUMBER=21445064; FIRST_ORDER_NEW_PRICE=10004; FIRST_ORDER_NEW_QUANTITY=4; SECOND_ORDER_NUMBER=21445065; SECOND_ORDER_NEW_PRICE=10004; SECOND_ORDER_NEW_QUANTITY=4;");
     }
     
     public static void main(String args[]) throws IOException {
@@ -171,12 +241,17 @@ public abstract class JavaToQuik {
                 System.out.println("onStop(" + reason + ")");
             }
         };
+        javaToQuik.setAccount("4100WWQ");
+        javaToQuik.setClientCode("4100WWQ");
+        
         System.in.read();
-        javaToQuik.sendTransaction("ACCOUNT=4100WWQ; CLIENT_CODE=4100WWQ; TYPE=M; TRANS_ID=8; CLASSCODE=SPBFUT; SECCODE=SRM6; ACTION=NEW_ORDER; OPERATION=S; PRICE=16231; QUANTITY=15;");
+        javaToQuik.sendOrder("SPBFUT", "SRM6", Operation.BUY, new BigDecimal("12680"), 1);
+        //javaToQuik.sendTransaction("ACCOUNT=" + javaToQuik.getAccount() + "; CLIENT_CODE=" + javaToQuik.getClientCode() + "; TYPE=M; CLASSCODE=SPBFUT; SECCODE=SRM6; ACTION=NEW_ORDER; OPERATION=S; PRICE=16231; QUANTITY=1;");
         
         System.in.read();
         // Удалить все заявки
-        javaToQuik.sendTransaction("TRANS_ID=4; CLASSCODE=SPBFUT; ACTION=KILL_ALL_ORDERS; CLIENT_CODE=4100WWQ;");
+        //javaToQuik.sendTransaction("CLASSCODE=SPBFUT; ACTION=KILL_ALL_ORDERS; CLIENT_CODE=" + javaToQuik.getClientCode() + ";");
+        javaToQuik.sendKillAllOrders("SPBFUT");
         System.in.read();
         javaToQuik.stop();
     }
